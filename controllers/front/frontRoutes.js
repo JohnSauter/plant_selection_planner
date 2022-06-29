eaac4739bd39020802eb96a8bbd14c98edb38e6c
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-06-29 14:17:30 -0400

    login page

--- controllers/front/frontRoutes.js
+++ controllers/front/frontRoutes.js
@@ -11,15 +11,11 @@ router.get('/', async (req, res) => {
 })
 
 /* More routes go here, then the API routes if we have them.
- * Planned routes are: get /front/login, post /front/login,
- * get /front/signup_as_gardener, post /front/signup_as_gardener,
- * get /front/signup_as_nursery_manager and
- * post /front/signup_as_nursery_manager.
   */
 
 /* No API routes yet.
 const apiRoutes = require('./api');
-router.use(apiRoutes);
+router.use('/api', apiRoutes);
 */
 
 module.exports = router;
