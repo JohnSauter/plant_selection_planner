f06917e5496731cba51bd88326fc8a4d73b93e01
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-06-30 16:52:44 -0400

    email needs secret information in environment variables to actually send the email message

--- public/js/gardener_home.js
+++ public/js/gardener_home.js
@@ -2,15 +2,20 @@
 const send_email = async () => {
   /* Tell the back end to email the selections to the
    * nursery manager.  */
-  const response = await fetch('/gardener/api/email', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-  });
+  try {
+    const response = await fetch('/gardener/api/email', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
 
-  if (response.ok) {
-    alert ('email sent')
-  } else {
-    alert('Failed to send email');
+    if (response.ok) {
+      alert('email sent');
+    } else {
+      alert('Failed to send email');
+    }
+  } catch (err) {
+    alert('email function failed');
+    console.log(err);
   }
 };
 
