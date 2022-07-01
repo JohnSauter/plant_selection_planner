bfbb1e1c4f5d71531cdab437c7d43d6538464b6e
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-07-01 17:34:37 -0400

    better error messages on gardener signup failure

--- controllers/user/api.js
+++ controllers/user/api.js
@@ -7,16 +7,87 @@ const {
   Garden_zone,
   Plant_type,
   Plant_instance,
-} = require('../../Models');
+} = require('../../models');
 const withAuth = require('../../utils/auth');
 
 /* Sign up as a gardener.  */
 router.post('/signup_as_gardener', async (req, res) => {
   try {
+    /* Do some validation of the signup information.  */
+    const password = req.body.password;
+    if (password.length < 8) {
+      res
+        .status(400)
+        .json({ error: 'Password must be at least 8 characters long.' });
+      return;
+    }
+    const email = req.body.email;
+    if (!email) {
+      res.status(400).json({ error: 'Email address is required.' });
+      return;
+    }
+    const email_validator_1 = new RegExp('^\\S+@\\S+\\.\\S+$', 'u');
+    if (!email_validator_1.test(email)) {
+      res.status(400).json({ error: 'Email address must have @ and .' });
+      return;
+    }
+    const email_validator_2 = new RegExp('^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$', 'u');
+    if (!email_validator_2.test(email)) {
+      res.status(400).json({ error: 'Email address must have only one @' });
+      return;
+    }
+    const email_validator_3 =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!email_validator_3.test(email)) {
+      res
+        .status(400)
+        .json({
+          error:
+            'Email address must have at least two characters' +
+            ' after the last dot.',
+        });
+      return;
+    }
+    const name = req.body.name;
+    if (!name) {
+      res.status(400).json({ error: 'Name may not be empty.' });
+      return;
+    }
+    const name_validator = new RegExp('^\\p{L}[\\p{L}\\p{Nd}_ ]*$', 'u');
+    if (!name_validator.test(name)) {
+      res
+        .status(400)
+        .json({
+          error:
+            'Name must start with a letter' +
+            ' and contain only letters, digits, underscores and spaces.',
+        });
+      return;
+    }
+    const other_user = await User.findAll({
+      where: {name: name}
+    });
+    if (other_user.length > 0) {
+      res
+        .status(400)
+        .json({
+          error:
+            'There is already somebody signed up' +
+            ' under the name ' +
+            name +
+            '.',
+        });
+      return;
+    }
+
+    /* Since he is signing up, log him out if he was previously
+     * logged in. */
+    req.session.logged_in = false;
+
     const userData = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
+      name: name,
+      email: email,
+      password: password,
       user_type: 'Gardener',
     });
 
@@ -29,7 +100,7 @@ router.post('/signup_as_gardener', async (req, res) => {
       res.status(201).json(userData);
     });
   } catch (err) {
-    res.status(400).json(err);
+    res.status(400).json({ error: 'Signup failed.', err: err });
   }
 });
 
