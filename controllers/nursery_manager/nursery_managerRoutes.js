aff8e298a57c169a544320206eef1222da60db47
AshCarr88 <101063900+AshCarr88@users.noreply.github.com>
2022-07-02 14:02:03 -0400

    Sent Manager API

--- controllers/nursery_manager/nursery_managerRoutes.js
+++ controllers/nursery_manager/nursery_managerRoutes.js
@@ -3,10 +3,17 @@ const { User, Garden_zone, Plant_type, Plant_instance } = require('../../Models'
 const withAuth = require('../../utils/auth');
 
 /* Routes go here, then the API routes when we have them.
+*/
 
 const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
-*/
+router.get('/home', (req, res) => {
+    res.render("nursery_manager_home");
+})
+
+
+
+
 
 module.exports = router;
