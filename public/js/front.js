21263a54745c3c3e35136f3751a07e80d93b4b3d
mendelism <hgustafson17@gmail.com>
2022-07-05 18:37:33 -0400

    search now works from front page

--- public/js/front.js
+++ public/js/front.js
@@ -23,3 +23,47 @@ if (sign_up_as_nursery_manager_btn) {
     sign_up_as_nursery_manager
   );
 }
+
+// Perform same as search.js but redirect to /search
+const plantSearchFormFront = document.querySelector('#plant-search-form');
+let searchCriteriaFront;
+
+const searchFormHandlerFront = (event) => {
+  event.preventDefault();
+
+  // Collect values from search form
+  const hardiness = document.querySelector('#hardiness-zone').value;
+  const habit = document.querySelector('#habit').value;
+  const lifeCycle = document.querySelector('#life-cycle').value;
+  const sunExposure = {
+    fullSun: document.querySelector('#full-sun').checked,
+    partSun: document.querySelector('#part-sun').checked,
+    partShade: document.querySelector('#part-shade').checked,
+    fullShade: document.querySelector('#full-shade').checked
+  };
+  const seasonOfInterest = {
+    earlySpring: document.querySelector('#early-spring').checked,
+    midSpring: document.querySelector('#mid-spring').checked,
+    lateSpring: document.querySelector('#late-spring').checked,
+    earlySummer: document.querySelector('#early-summer').checked,
+    midSummer: document.querySelector('#mid-summer').checked,
+    lateSummer: document.querySelector('#late-summer').checked,
+    fall: document.querySelector('#fall').checked,
+    winter: document.querySelector('#winter').checked
+    
+  }
+
+  // Consolidate search values into a single object
+  searchCriteria = {
+    hardiness,
+    habit,
+    lifeCycle,
+    sunExposure,
+    seasonOfInterest
+  };
+
+  localStorage.setItem("userSearch", JSON.stringify(searchCriteria));
+  location.replace('/search');
+};
+
+plantSearchFormFront.addEventListener('submit', searchFormHandlerFront);
