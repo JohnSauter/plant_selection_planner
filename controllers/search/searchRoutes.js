551b8af6e7614e9202de09c8feeeb088e0422762
mendelism <hgustafson17@gmail.com>
2022-07-04 23:57:42 -0400

    results display

--- controllers/search/searchRoutes.js
+++ controllers/search/searchRoutes.js
@@ -8,7 +8,6 @@ const {
   Plant_instance,
 } = require('../../models');
 const withAuth = require('../../utils/auth');
-const { Op } = require("sequelize");
 
 router.get('/', async (req, res) => {
   const plantData = await Plant_type.findAll();
@@ -25,88 +24,8 @@ router.get('/results', async (req, res) => {
   });
 });
 
-router.get('/:criteria', async (req, res) => {
-  try {
-    const criteria = req.params.criteria
-    const searchCriteria = JSON.parse(criteria);
-    const plantData = await Plant_type.findAll({
-      where: {
-        hardiness_zone_lower: {
-          [Op.lte]: searchCriteria.hardiness,
-        },
-        hardiness_zone_upper: {
-          [Op.gte]: searchCriteria.hardiness,
-        },
-        habit: searchCriteria.habit,
-        life_cycle: searchCriteria.lifeCycle,
-        // full_sun: searchCriteria.fullSun,
-        // part_sun: searchCriteria.partSun,
-        // part_shade: searchCriteria.partShade,
-        // full_shade: searchCriteria.fullShade,
-        // early_spring: searchCriteria.earlySpring,
-        // mid_spring: searchCriteria.midSpring,
-        // late_spring: searchCriteria.lateSpring,
-        // early_summer: searchCriteria.earlySummer,
-        // mid_summer: searchCriteria.midSummer,
-        // late_summer: searchCriteria.lateSummer,
-        // fall: searchCriteria.fall,
-        // winter: searchCriteria.winter,
-      }
-    });
-    const plants = plantData.map((plant) => plant.get({ plain: true }));
-    console.log(plants);
-    res.json(plants);
 
-    res.render('search', { plants });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.post('/', async (req, res) => {
-  try {
-    if (req.body) {
-      const searchCriteria = req.body.criteria;
-      const plantData = await Plant_type.findAll({
-        where: {
-          hardiness_zone_lower: {
-            [Op.lte]: searchCriteria.hardiness,
-          },
-          hardiness_zone_upper: {
-            [Op.gte]: searchCriteria.hardiness,
-          },
-          habit: searchCriteria.habit,
-          life_cycle: searchCriteria.lifeCycle,
-          // full_sun: searchCriteria.fullSun,
-          // part_sun: searchCriteria.partSun,
-          // part_shade: searchCriteria.partShade,
-          // full_shade: searchCriteria.fullShade,
-          // early_spring: searchCriteria.earlySpring,
-          // mid_spring: searchCriteria.midSpring,
-          // late_spring: searchCriteria.lateSpring,
-          // early_summer: searchCriteria.earlySummer,
-          // mid_summer: searchCriteria.midSummer,
-          // late_summer: searchCriteria.lateSummer,
-          // fall: searchCriteria.fall,
-          // winter: searchCriteria.winter,
-        }
-      });
-      const plants = plantData.map((plant) => plant.get({ plain: true }));
-      console.log(plants);
-    res.json(plants);
-  }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Retrieve a plant by its id
-router.get('/:id', async (req, res) => {
-  const plantData = await Plant_type.findByPk(req.params.id)
-
-  const plant = plantData.get({ plain: true });
-
-  console.log(plant);
-});
+const apiRoutes = require('./api');
+router.use('/api', apiRoutes);
 
 module.exports = router;
